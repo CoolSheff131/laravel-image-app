7f809f721ff65a8ce9a69a285d52a4d24ecebd5a
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-03-22 22:22:03 +0800

    vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+import Vue from "vue";
+import Index from "./components/Index.vue";
+require("./bootstrap");
+
+const app = new Vue({
+    el: "#app",
+    components: {
+        Index,
+    },
+});
